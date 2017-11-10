72ea592d73b1c611c41bd6859db03347172fedc3
Michael <szalmichael@gmail.com>
2017-11-10 14:36:19 -0600

    Added SpaceMap Instance

--- eclipse-workspace/TreasureHunter/src/Game.java
+++ eclipse-workspace/TreasureHunter/src/Game.java
@@ -1,3 +1,4 @@
+import javafx.geometry.Point2D;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -11,10 +12,17 @@ public class Game extends Application {
 	public final int GRID_SIZE		  = 500;
 	public final int SCREEN_GRID_SIZE = 10;
 	
+	private SpaceMap spaceMap;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		Pane root = new AnchorPane();
 		Scene scene = new Scene(root,500,500);
+		
+		spaceMap = SpaceMap.getInstance();
+		
+		//spaceMap.setInhabitant(Inhabitant.ALIEN, new Point2D(10,10));
+		
 		
 		primaryStage.setTitle("Space Treasure Hunter");
 		primaryStage.setScene(scene);
