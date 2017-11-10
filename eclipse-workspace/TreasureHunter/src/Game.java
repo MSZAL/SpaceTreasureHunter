0f22e67b437c68c79dc30173061c87a70141f949
Michael <szalmichael@gmail.com>
2017-11-10 14:00:38 -0600

    Added JafaFX essentials

--- eclipse-workspace/TreasureHunter/src/Game.java
+++ eclipse-workspace/TreasureHunter/src/Game.java
@@ -1,4 +1,40 @@
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class Game {
+public class Game extends Application {
+	
+	public final int GRID_SIZE		  = 500;
+	public final int SCREEN_GRID_SIZE = 10;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Pane root = new AnchorPane();
+		Scene scene = new Scene(root,500,500);
+		
+		primaryStage.setTitle("Space Treasure Hunter");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		EventHandler<KeyEvent> keyHandler = new EventHandler<KeyEvent>() {
 
+			@Override
+			public void handle(KeyEvent event) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		};
+		
+		scene.setOnKeyPressed(keyHandler);
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
