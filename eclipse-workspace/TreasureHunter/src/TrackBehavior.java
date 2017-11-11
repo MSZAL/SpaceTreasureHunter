0dc8c6a11757b13647f479fb7d32793b0ec771bb
Michael <szalmichael@gmail.com>
2017-11-10 19:49:34 -0600

    Added Constructor. Need Player Class Finished.

--- eclipse-workspace/TreasureHunter/src/TrackBehavior.java
+++ eclipse-workspace/TreasureHunter/src/TrackBehavior.java
@@ -2,6 +2,10 @@ import javafx.geometry.Point2D;
 
 public class TrackBehavior implements Behavior {
 
+	public TrackBehavior(/*Player player*/) {
+		
+	}
+	
 	@Override
 	public Point2D nextMove() {
 		// TODO Auto-generated method stub
